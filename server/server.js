efa2615391f7668216ae58ebbd134f072e6ab4bb
Abdul Wahab <abdulwahabee5@gmail.com>
2025-08-13 11:58:04 +0500

    ddd

--- server/server.js
+++ server/server.js
@@ -22,12 +22,7 @@ mongoose.connect('mongodb+srv://abdulwahabeeee5:abdulwahabatlas@cluster0.mujzo.m
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({
-    origin: 'http://localhost:5173',
-    methods : ['GET','POST','DELETE','PUT'],
-    allowedHeaders : ["Content-Type","Authorization","Cache-Control","Expires","Pragma"],
-    credentials : true
-}))
+app.use(cors())
 
 app.use(cookieParser())
 app.use(express.json())
